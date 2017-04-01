ff8eb534e74dae3f59ce0b5fb3f0b5488154b312
Sven Luijten <svenluijten1995@gmail.com>
2017-04-01 20:03:03 +0200

    Interactively ask the user for their API key

--- src/Commands/Authorize.php
+++ src/Commands/Authorize.php
@@ -3,7 +3,7 @@
 namespace Sven\ForgeCLI\Commands;
 
 use Sven\ForgeCLI\Config;
-use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Question\Question;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -21,7 +21,6 @@ class Authorize extends BaseCommand
     {
         $this->setName('authorize')
             ->setDescription('Set or update the API key used in the commands.')
-            ->addArgument('key', InputArgument::REQUIRED, 'The API key found in your Forge account.')
         ;
     }
 
@@ -30,7 +29,11 @@ class Authorize extends BaseCommand
      */
     public function perform(InputInterface $input, OutputInterface $output)
     {
-        (new Config)->set('key', $input->getArgument('key'));
+        $helper = $this->getHelper('question');
+        $question = new Question('What is your API key? ');
+        $question->setHidden(true);
+
+        (new Config)->set('key', $helper->ask($input, $output, $question));
 
         $output->write('<info>Your API key has successfully been set.</info>');
     }
