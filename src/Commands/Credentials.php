d4e5af0e29ef0842acb902487973d38d6c2d1b68
Sven Luijten <svenluijten1995@gmail.com>
2017-04-01 19:32:19 +0200

    Add credentials command

---
+++ src/Commands/Credentials.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sven\ForgeCLI\Commands;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Themsaid\Forge\Resources\Credential;
+
+class Credentials extends BaseCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configure()
+    {
+        $this->setName('credentials')
+            ->setDescription('Show all credentials listed in your account.')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function perform(InputInterface $input, OutputInterface $output)
+    {
+        $this->table($output, ['Id', 'Name', 'Type'], array_map(function (Credential $credential) {
+            return [$credential->id, $credential->name, $credential->type];
+        }, $this->forge->credentials()));
+    }
+}
