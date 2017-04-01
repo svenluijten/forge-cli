50dc681d1dddcfde785975384c2eea0233b5b9be
Sven Luijten <svenluijten1995@gmail.com>
2017-04-01 19:48:41 +0200

    Implement create:server command

--- src/Commands/Servers/Create.php
+++ src/Commands/Servers/Create.php
@@ -3,18 +3,46 @@
 namespace Sven\ForgeCLI\Commands\Servers;
 
 use Sven\ForgeCLI\Commands\BaseCommand;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Create extends BaseCommand
 {
     /**
+     * @var array
+     */
+    protected $optionMap = [
+        'provider' => 'provider',
+        'credentials' => 'credential_id',
+        'region' => 'region',
+        'ip' => 'ip_address',
+        'private-ip' => 'private_ip_address',
+        'php' => 'php_version',
+        'database' => 'database',
+        'install-maria' => 'maria',
+        'load-balancer' => 'load_balancer',
+        'network' => 'network',
+    ];
+
+    /**
      * {@inheritdoc}
      */
     public function configure()
     {
         $this->setName('create:server')
-            ->setDescription('Create a new server.');
+            ->setDescription('Create a new server.')
+            ->addOption('provider', null, InputOption::VALUE_REQUIRED, 'The provider to provision the server on. Can be either "ocean2", "linode", "aws", or "custom".', 'ocean2')
+            ->addOption('credential', 'C', InputOption::VALUE_REQUIRED, 'Credentials to use. Not required when using the "custom" provider.')
+            ->addOption('region', null, InputOption::VALUE_REQUIRED, 'The region to provision the server in. Not required when using the "custom" provider.')
+            ->addOption('ip', null, InputOption::VALUE_REQUIRED, 'IP address of the server. Required when using the "custom" provider.')
+            ->addOption('private-ip', null, InputOption::VALUE_REQUIRED, 'Private IP address of the server. Required when using the "custom" provider.')
+            ->addOption('php', null, InputOption::VALUE_REQUIRED, 'PHP version to install when provisioning the server. Supported versions are "php56", "php70", and "php71".')
+            ->addOption('database', null, InputOption::VALUE_REQUIRED, 'The name of the database to create when provisioning the server.', 'forge')
+            ->addOption('install-maria', null, InputOption::VALUE_NONE, 'Whether MariaDB should be installed. If left out, MySQL will be installed by default.')
+            ->addOption('load-balancer', null, InputOption::VALUE_NONE, 'Whether the server should be provisioned as a load balancer.')
+            ->addOption('network', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED, 'Other servers\' ids this one can network with.')
+        ;
     }
 
     /**
@@ -22,6 +50,8 @@ class Create extends BaseCommand
      */
     public function perform(InputInterface $input, OutputInterface $output)
     {
-        //
+        $this->forge->createServer(
+            $this->fillData($input->getOptions())
+        );
     }
 }
