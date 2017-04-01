cbd7cf8a9a9d2788191d1e25184e268c16968880
Sven Luijten <svenluijten1995@gmail.com>
2017-04-01 20:33:48 +0200

    Implement list:sites command

---
+++ src/Commands/Sites/All.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Sven\ForgeCLI\Commands\Sites;
+
+use Themsaid\Forge\Resources\Site;
+use Sven\ForgeCLI\Commands\BaseCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class All extends BaseCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configure()
+    {
+        $this->setName('list:sites')
+            ->setDescription('Show all sites installed on a server.')
+            ->addArgument('server', InputArgument::REQUIRED, 'The id of the server to list the sites for.')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function perform(InputInterface $input, OutputInterface $output)
+    {
+        $this->table($output, ['Id', 'Name', 'Type', 'Repository', 'Branch'], array_map(function (Site $site) {
+            return [$site->id, $site->name, $site->projectType, $site->repository ?: '-', $site->repositoryBranch ?: '-'];
+        }, $this->forge->sites($input->getArgument('server'))));
+    }
+}
