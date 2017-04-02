5fd8361e406a2be11348169bf9cecdea27d0b651
Sven Luijten <svenluijten1995@gmail.com>
2017-04-02 16:38:19 +0200

    Impelement service:stop command

---
+++ src/Commands/Services/Stop.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Sven\ForgeCLI\Commands\Services;
+
+use Sven\ForgeCLI\Commands\BaseCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Stop extends BaseCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configure()
+    {
+        $this->setName('stop:service')
+            ->setDescription('Stop a service on a server.')
+            ->addArgument('service', InputArgument::REQUIRED, 'The service to stop. Can be either "nginx", "mysql" or "postgres".')
+            ->addArgument('server', InputArgument::REQUIRED, 'The id of the server to stop the service on.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function perform(InputInterface $input, OutputInterface $output)
+    {
+        $service = strtolower($input->getArgument('service'));
+        $server = $input->getArgument('server');
+
+        switch ($service) {
+            case 'mysql':
+                $this->forge->stopMysql($server);
+                break;
+            case 'nginx':
+                $this->forge->stopNginx($server);
+                break;
+            case 'postgres':
+                $this->forge->stopPostgres($server);
+                break;
+        }
+    }
+}
