3f7fddec20bee4566140a987612beabf7ee4c958
Sven Luijten <svenluijten1995@gmail.com>
2017-04-02 16:36:59 +0200

    Implement service:reboot command

---
+++ src/Commands/Services/Reboot.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Sven\ForgeCLI\Commands\Services;
+
+use Sven\ForgeCLI\Commands\BaseCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Reboot extends BaseCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function configure()
+    {
+        $this->setName('reboot:service')
+            ->setDescription('Reboot a service on a server.')
+            ->addArgument('service', InputArgument::REQUIRED, 'The service to reboot. Can be either "nginx", "mysql" or "postgres".')
+            ->addArgument('server', InputArgument::REQUIRED, 'The id of the server to reboot the service on.');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function perform(InputInterface $input, OutputInterface $output)
+    {
+        $service = strtolower($input->getArgument('service'));
+        $server = $input->getArgument('server');
+
+        switch ($service) {
+            case 'mysql':
+                $this->forge->rebootMysql($server);
+                break;
+            case 'nginx':
+                $this->forge->rebootNginx($server);
+                break;
+            case 'postgres':
+                $this->forge->rebootPostgres($server);
+                break;
+        }
+    }
+}
