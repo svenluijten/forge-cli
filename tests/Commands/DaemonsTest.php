b6a734278deb556a295b3b24e35852c4b9311a29
Sven Luijten <svenluijten1995@gmail.com>
2018-01-27 22:36:24 +0100

    add show:daemon test

--- tests/Commands/DaemonsTest.php
+++ tests/Commands/DaemonsTest.php
@@ -6,6 +6,7 @@ use Sven\ForgeCLI\Commands\Daemons\All;
 use Sven\ForgeCLI\Commands\Daemons\Delete;
 use Sven\ForgeCLI\Commands\Daemons\Make;
 use Sven\ForgeCLI\Commands\Daemons\Reboot;
+use Sven\ForgeCLI\Commands\Daemons\Show;
 use Sven\ForgeCLI\Tests\TestCase;
 use Themsaid\Forge\Resources\Daemon;
 
@@ -34,7 +35,7 @@ class DaemonsTest extends TestCase
     }
 
     /** @test */
-    public function it_deltes_a_daemon()
+    public function it_deletes_a_daemon()
     {
         $this->forge->expects($this->once())
             ->method('deleteDaemon')
@@ -104,6 +105,23 @@ class DaemonsTest extends TestCase
 
         $this->command(Reboot::class)
             ->setInputs(['no'])
+            ->execute([
+                'server' => '12345',
+                'daemon' => '67890',
+            ]);
+    }
+
+    /** @test */
+    public function it_shows_information_about_a_daemon()
+    {
+        $this->forge->expects($this->once())
+            ->method('daemon')
+            ->with('12345', '67890')
+            ->willReturn(
+                new Daemon(['id' => '67890', 'command' => 'echo \'hello world\' >> /dev/null'])
+            );
+
+        $this->command(Show::class)
             ->execute([
                 'server' => '12345',
                 'daemon' => '67890',
