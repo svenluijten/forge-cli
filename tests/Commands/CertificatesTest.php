0be4541a66871f933e8dbc24197ddb81a2f6f091
Sven Luijten <svenluijten1995@gmail.com>
2018-05-09 23:46:11 +0200

    test activation of certificate

---
+++ tests/Commands/CertificatesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sven\ForgeCLI\Tests\Commands;
+
+use Sven\ForgeCLI\Commands\Certificates\Activate;
+use Sven\ForgeCLI\Tests\TestCase;
+use Themsaid\Forge\Resources\Certificate;
+
+class CertificatesTest extends TestCase
+{
+    /** @test */
+    public function it_can_activate_a_certificate()
+    {
+        $this->forge->shouldReceive()
+            ->activateCertificate('12345', '67890', '13579', false)
+            ->once()
+            ->andReturn([
+                new Certificate(['id' => '13579', 'serverId' => '12345', 'siteId' => '67890']),
+            ]);
+
+        $tester = $this->command(Activate::class);
+
+        $tester->execute([
+            'server' => '12345',
+            'site' => '67890',
+            'certificate' => '13579',
+        ]);
+    }
+}
