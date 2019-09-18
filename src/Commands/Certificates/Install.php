835baa2ac0598240b9120e8c6ef9b963a6fb1941
Thore Suenert <mail@thoresuenert.de>
2019-09-18 15:50:54 +0200

    Add optional wait parameter for installing certificate

--- src/Commands/Certificates/Install.php
+++ src/Commands/Certificates/Install.php
@@ -5,6 +5,7 @@ namespace Sven\ForgeCLI\Commands\Certificates;
 use Sven\ForgeCLI\Commands\BaseCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class Install extends BaseCommand
@@ -16,8 +17,9 @@ class Install extends BaseCommand
     {
         $this->setName('install:certificate')
             ->addArgument('server', InputArgument::REQUIRED, 'The id of the server where the site is.')
-            ->addArgument('site', InputArgument::REQUIRED, 'The id of the site the SSL certificate to delete is on.')
-            ->addArgument('certificate', InputArgument::REQUIRED, 'The id of the SSL certificate to delete.')
+            ->addArgument('site', InputArgument::REQUIRED, 'The id of the site the SSL certificate to install is on.')
+            ->addArgument('certificate', InputArgument::REQUIRED, 'The id of the SSL certificate to install.')
+            ->addOption('wait', null, InputOption::VALUE_OPTIONAL, 'Boolean if you want to wait for execution', false)
             ->setDescription('Install the given SSL certificate on a site.');
     }
 
@@ -27,7 +29,7 @@ class Install extends BaseCommand
     public function execute(InputInterface $input, OutputInterface $output)
     {
         $this->forge->installCertificate(
-            $input->getArgument('server'), $input->getArgument('site'), $input->getArgument('certificate')
+            $input->getArgument('server'), $input->getArgument('site'), $input->getArgument('certificate'), [] , ($input->getOption('wait') !== false)
         );
     }
 }
