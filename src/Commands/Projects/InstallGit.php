4d39bc29376fbce37b84600b462b5fd2d7233a51
Thore Suenert <mail@thoresuenert.de>
2019-09-18 16:04:14 +0200

    Add optional wait paramter for install git repo

--- src/Commands/Projects/InstallGit.php
+++ src/Commands/Projects/InstallGit.php
@@ -30,6 +30,7 @@ class InstallGit extends BaseCommand
             ->addOption('provider', null, InputOption::VALUE_REQUIRED, 'The provider to use. Must be either "github" or "custom".', 'github')
             ->addOption('repository', null, InputOption::VALUE_REQUIRED, 'The repository to install. Must be in the format "username/repository".')
             ->addOption('branch', null, InputOption::VALUE_OPTIONAL, 'The branch to install. Must be in the format "master".', 'master')
+            ->addOption('wait', null, InputOption::VALUE_OPTIONAL, 'Boolean if you want to wait for execution', false)
             ->setDescription('Install a new git project on a site.');
     }
 
@@ -41,7 +42,8 @@ class InstallGit extends BaseCommand
         $this->forge->installGitRepositoryOnSite(
             $input->getArgument('server'),
             $input->getArgument('site'),
-            $this->fillData($input->getOptions())
+            $this->fillData($input->getOptions()),
+            ($input->getOption('wait') !== false)
         );
     }
 }
