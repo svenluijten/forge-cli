46c1a288fb2816942d4844aa2c3f519647bf4ef6
Thore Suenert <mail@thoresuenert.de>
2019-09-19 11:03:46 +0200

    Add missing import inputoption to worker reboot

--- src/Commands/Workers/Reboot.php
+++ src/Commands/Workers/Reboot.php
@@ -5,6 +5,7 @@ namespace Sven\ForgeCLI\Commands\Workers;
 use Sven\ForgeCLI\Commands\BaseCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 
