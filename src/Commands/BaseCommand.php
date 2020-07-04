d51940104f5aa5ff381a0aa90d5dee26714ca6d3
Sven Luijten <svenluijten1995@gmail.com>
2020-07-04 14:30:43 +0200

    Please StyleCI

--- src/Commands/BaseCommand.php
+++ src/Commands/BaseCommand.php
@@ -144,7 +144,7 @@ abstract class BaseCommand extends Command
         // If this is the first time this command is run, we will
         // create a new configuration file. Otherwise, we just
         // return the already existing configuration store.
-        if (! file_exists($config)) {
+        if (!file_exists($config)) {
             file_put_contents($config, '{"key":""}');
         }
 
